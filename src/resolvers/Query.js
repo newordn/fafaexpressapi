3c275d1f0cd14a231e5f5c4768e677c90f6b639b
Neword <newordn@gmail.com>
2020-07-06 17:07:30 -0700

    added orderedPlatesByUser, orderedHousesByUser, and orderedProductsByUser queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -9,10 +9,28 @@ const users = async (parent,args,context,info)=>{
     const users = await context.prisma.users({orderBy:'id_DESC'})
     return users
 }
+const orderedPlatesByUser = async (parent,args,context,info)=>{
+    console.log('orderedPlatesByUser query')
+    const orderedPlates = await context.prisma.usersOnPlateses({where:{user:{id:args.user}}})
+    return orderedPlates
+}
+const orderedHousesByUser = async (parent,args,context,info)=>{
+    console.log('orderedHousesByUser query')
+    const orderedHousesByUser = await context.prisma.usersOnHouseses({where:{user:{id:args.user}}})
+    return orderedHousesByUser
+}
+const orderedProductsByUser = async (parent,args,context,info)=>{
+    console.log('orderedProductsByUser query')
+    const orderedProductsByUser = await context.prisma.usersOnProductses({where:{user:{id:args.user}}})
+    return orderedProductsByUser
+}
 
 
 module.exports={
     info,
     steedsByUser,
-    users
+    users,
+    orderedPlatesByUser,
+    orderedHousesByUser,
+    orderedProductsByUser
 }
