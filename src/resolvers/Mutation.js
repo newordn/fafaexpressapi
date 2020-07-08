e9e64d4aa2376d1e99f015d03221bbe63a6f15a4
Neword <newordn@gmail.com>
2020-07-08 15:50:50 -0700

    fix

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -23,7 +23,7 @@ const signUp = async (parent,args,context,info)=>{
     const password = await bcrypt.hash(args.password,10)
     const illustration =  await context.storeUpload(args.illustration)
     const user = await context.prisma.createUser({...args,password,illustration:illustration.path})
-    //await sendMail(user.email,"FafaExpress",`Inscription Reussite. Bienvenu dans FafaExpress, faites vous livrez en un laps de temps.`)
+    await sendMail(user.email,"FafaExpress",`Inscription Reussite. Bienvenu dans FafaExpress, faites vous livrez en un laps de temps.`)
     return user
 }
 const plat = async (parent,args,context,info)=>{
